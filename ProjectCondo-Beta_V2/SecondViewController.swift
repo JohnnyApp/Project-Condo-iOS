f5d4343d97ef88728b858edbbf541cd12b23706a
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2017-06-18 22:13:23 -0700

    Initial Commit

---
+++ ProjectCondo-Beta_V2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ProjectCondo-Beta_V2
+//
+//  Created by jonathan laroco on 6/18/17.
+//  Copyright © 2017 jonathan laroco. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

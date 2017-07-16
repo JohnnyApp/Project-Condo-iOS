d3cf2403e4dc8485560ba0e5cf2ac27607c530c5
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2017-07-15 22:37:36 -0700

    Working version with AWS Test Instance

--- RestAPIEngine.swift
+++ RestAPIEngine.swift
@@ -11,8 +11,11 @@ import UIKit
 import Foundation
 
 //Server Setup Parameters -
-private let ApiKey = "0e72faf8133c347beec46d2204a96d3c1e025a328ebc9c0f3f2a2767bf5d5d4f"
-private let BaseInstanceUrl = "http://localhost:8080/api/v2"
+//private let ApiKey = "0e72faf8133c347beec46d2204a96d3c1e025a328ebc9c0f3f2a2767bf5d5d4f"
+private let ApiKey = "4be93293a709f0bbad4e5f6da89130c870889c34471ad9849ee8a74c52336c2a"
+//private let BaseInstanceUrl = "http://localhost:8080/api/v2"
+private let BaseInstanceUrl = "http://ec2-52-37-95-16.us-west-2.compute.amazonaws.com/api/v2"
+//private let UserRegisterExtension = "/user/register?login=true"
 private let UserRegisterExtension = "/user/register?login=true"
 private let UserLoginExtension = "/user/session"
 private let kSessionTokenKey = "SessionToken"
@@ -137,6 +140,8 @@ final class RESTAPIEngine {
                 }
                 //Check Errors +
                 
+                print("Output message" + outputMsg)
+                print(data)
                 
                 //Successful create a new session -
                 if outputMsg == "" {
@@ -144,6 +149,7 @@ final class RESTAPIEngine {
                     
                     if (outputDict["session_token"] != nil) {
                         //Add Session Token/User Defaults -
+                        print(outputDict["session_token"] as? String ?? "NOTHING")
                         self.sessionToken = outputDict["session_token"] as? String
                         let defaults = UserDefaults.standard
                         defaults.setValue(_username, forKey: "username")

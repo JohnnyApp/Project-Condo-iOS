90f6309131acd370eb6a5dbf8653ffb0a5705c86
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2017-09-02 13:35:07 -0700

    Commit

---
+++ ProjectCondo-Beta_V2/CreateHouseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateHouseViewController.swift
+//  ProjectCondo-Beta_V2
+//
+//  Created by jonathan laroco on 8/11/17.
+//  Copyright © 2017 jonathan laroco. All rights reserved.
+//
+
+import UIKit
+
+class CreateHouseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
